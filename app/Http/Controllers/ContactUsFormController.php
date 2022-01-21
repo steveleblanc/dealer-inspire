43becdfbe631e7f6f5111ff3851383c29b51bac5
steveleblanc <steveleblanc77@gmail.com>
2022-01-20 19:16:17 -0500

    Created model contoller and db migration files

---
+++ app/Http/Controllers/ContactUsFormController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ContactUsForm;
+use Illuminate\Http\Request;
+
+class ContactUsFormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ContactUsForm  $contactUsForm
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ContactUsForm $contactUsForm)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ContactUsForm  $contactUsForm
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ContactUsForm $contactUsForm)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ContactUsForm  $contactUsForm
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ContactUsForm $contactUsForm)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ContactUsForm  $contactUsForm
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ContactUsForm $contactUsForm)
+    {
+        //
+    }
+}
